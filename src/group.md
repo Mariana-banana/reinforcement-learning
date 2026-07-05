71422625132193eecebdede0b4a930bdbf5e49f6
Aline Fraga <aline.fraga@poatek.com>
2026-07-05 10:36:39 -0300

    Implementa questão 4 e 5, adiciona group.md solicitado

---
+++ src/group.md
@@ -0,0 +1,8 @@
+# Grupo
+
+**Turma:** B
+
+**Integrantes:**
+- Aline Fraga da Silva — 579723
+- Gabriel Ricci Pazinatto — 578935
+- Mariana Burzlaff Ercolani — 579108
--- src/qlearningAgents.py
+++ src/qlearningAgents.py
@@ -92,8 +92,11 @@ class QLearningAgent(ReinforcementAgent):
         # Pick Action
         legalActions = self.getLegalActions(state)
         action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if legalActions:
+            if util.flipCoin(self.epsilon):
+                action = random.choice(legalActions)
+            else:
+                action = self.computeActionFromQValues(state)
 
         return action
 
